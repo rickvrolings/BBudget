e19e8da100179bfbef1c6dfb75ffb826862f4d36
Rick Vrolings <416994@student.fontys.nl>
2020-01-06 12:08:50 +0100

    Init2

---
+++ BBudget-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BBudget_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
